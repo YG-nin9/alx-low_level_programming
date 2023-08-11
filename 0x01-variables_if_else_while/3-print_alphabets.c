6195665c4b8125a57346328f027ef417f7a81522
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-11 13:19:12 -0700

    alpha & ALPHA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	/* your code goes there */
+	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+'x', 'y', 'z'};
+	char ALPHA[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+'X', 'Y', 'Z'};
+	int count = 0;
+	int COUNT = 0;
+
+	for (; count < 26;)
+	{
+		printf("%c", alpha[count]);
+		count++;
+	}
+	printf("\n");
+	for (; COUNT < 26;)
+        {
+                printf("%c", ALPHA[COUNT]);
+                COUNT++;
+        }
+        printf("\n");
+	return (0);
+}
+
