677990fde90167dc006fee7d6eedb49fbd93f2fe
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-11 13:25:55 -0700

    racist alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	/* your code goes there */
+	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+'x', 'y', 'z'};
+	int count = 0;
+
+	for (; count < 26;)
+	{
+		if (count == || count == )
+		{
+			count++;
+		}
+		printf("%c", alpha[count]);
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
+
