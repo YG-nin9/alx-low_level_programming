6e1ef0c9548288cfbc626bfecbeb7faf99ccbdf9
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-11 13:50:11 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	/* your code goes there */
+	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+'x', 'y', 'z'};
+	int count = 26;
+
+	for (; count >= 0;)
+	{
+		putchar(alpha[count]);
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
+
