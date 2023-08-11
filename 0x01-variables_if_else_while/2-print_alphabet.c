71a8cf4d7983f7cb1ff63c5498bf9ac2e4833fcd
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-11 13:12:05 -0700

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,8 +11,9 @@
 int main(void)
 {
 	/* your code goes there */
-	char alpha [26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char alpha [26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+'x', 'y', 'z'};
 	int count = 0;
 
 	for (; count < 26;)
