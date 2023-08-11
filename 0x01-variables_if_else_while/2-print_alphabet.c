c413fbc1b80aa8f47b0a58d84895890602f630fc
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-11 12:49:04 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	/* your code goes there */
+	char alpha = [a..z];
+	int count = 0;
+
+	for (;count != 25;)
+	{
+		printf("%c");
+		count ++;
+	}
+	return (0);
+}
+
