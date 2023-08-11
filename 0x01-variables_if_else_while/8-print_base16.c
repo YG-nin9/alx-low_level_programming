1e76efc2c59a779f60751ac54a6411c0eae6f35e
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-11 14:02:49 -0700

    hexc

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	/* your code goes there */
+	int count = 0;
+	int alph = 0;
+
+	for (; count < 10;)
+	{
+		if (count == 10)
+		{
+			for (;alph < 7;)
+			{
+				putchar(97 + alph);
+		
+			}
+		}
+		else 
+		{
+			break;
+		}
+		putchar(48 + count);
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
+
