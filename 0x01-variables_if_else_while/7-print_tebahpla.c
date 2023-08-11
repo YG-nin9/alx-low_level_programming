9ef3fde46ffdaa0daa759699235c3a50927b6f7b
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-11 13:54:10 -0700

    reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -14,12 +14,12 @@ int main(void)
 	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
 'x', 'y', 'z'};
-	int count = 26;
+	int count = 25;
 
 	for (; count >= 0;)
 	{
 		putchar(alpha[count]);
-		count++;
+		count--;
 	}
 	putchar('\n');
 	return (0);
