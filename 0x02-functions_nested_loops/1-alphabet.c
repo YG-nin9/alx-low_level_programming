06ae0b472e7998741de12e2a969c77e605bf75fd
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-15 04:10:58 -0700

    put alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,6 @@
 /* more headers goes there */
 #include <stdio.h>
 #include "main.h"
-/* betty style doc for function main goes there */
-/**
-*main - Entry point
-*
-*Return: Always 0 (Success)
-*/
 /**
 *Print_alphabet - print all alphabet in lowercase
 */
