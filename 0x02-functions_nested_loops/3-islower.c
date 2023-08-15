0c52b3387caa295c5e9f0a35f9c344a082e09c15
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-15 06:25:17 -0700

    lower??

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/**
+*_islower - checks the lower chars
+*/
+int _islower(int c);
+{
+	/* your code goes there */
+	char out;
+
+	if(out >= 'a' && out <= 'z')
+	{
+		return(1);
+	}
+	else 
+	{
+		return(0);
+	}
+}
+
