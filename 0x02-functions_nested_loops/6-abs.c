f4c399125461b988cc8801f3ff612ef4856bf6ca
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-15 07:06:30 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/**
+*_abs - absolute the value
+*@n: the input
+*Return: n
+*/
+int _abs(int n)
+{
+	/* your code goes there */
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
+
