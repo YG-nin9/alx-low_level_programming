769f26381f59f2ad68f95e6a949998d787dff07c
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-15 06:49:52 -0700

    alpha??

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/**
+*print_sign - checks the numbers
+*@n: the input
+*Return: 1 if out is lowercase, otherwise 0
+*/
+int print_sign(int n);
+{
+	/* your code goes there */
+	if (n > 0)
+	{
+		return (1);
+		putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		putchar('0');
+	}
+	else 
+	{
+		return(-1);
+		putchar('-');
+	}
+}
+
