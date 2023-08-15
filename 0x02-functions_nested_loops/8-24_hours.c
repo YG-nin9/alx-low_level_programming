7bd60fff7211ea888888d8c6b36808f9193120fd
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-15 08:00:46 -0700

    day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,7 +21,7 @@ void jack_bauer(void)
 			_putchar((hours % 10) + '0');
 			_putchar(':');
 			_putchar((minutes / 10) + '0');
-			_putchar((minutess % 10) + '0');
+			_putchar((minutes % 10) + '0');
 			_putchar('\n');
 			minutes++;
 		}
