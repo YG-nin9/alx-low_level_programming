e3d8fba8c5a4cb45e5201011ee197a34a662d811
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-15 01:22:58 -0700

    put

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	/* your code goes there */
+	char out[] = "_putchar";
+	int count = 0;
+
+	for (; count < 8; count++)
+	{
+		putchar(out[count]);
+	}
+	printf("\n");
+	return (0);
+}
+
