dff6741d7722e2c7bc6fbb0a0ddd9b636679428a
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-15 03:05:10 -0700

    put

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,32 +10,4 @@ int _putchar(char c)
 	{
 		return (write(1, &c, 1));
 	}
-
-void print_alphabet(void)
-    {
-    char out[] = "abcdefghijklmnopqrstuvwxyz";
-	int count = 0;
-
-	for (; count < 27; count++)
-	{
-		putchar(out[count]);
-	}
-	printf("\n");
-    }
-
-void print_alphabet_x10(void)
-	{
-	char out[] = "abcdefghijklmnopqrstuvwxyz";
-	int re =0;
-	int count = 0;
-
-	for(;re < 10; re++)
-	 {
-		 for (; count < 27; count++)
-		 {
-			 putchar(out[count]);
-		 }
-		 printf("\n");
-	 }
-	}
 #endif
