275f53f06adee773a2a1845339648c74537d4796
YG-nin9 <138725321+YG-nin9@users.noreply.github.com>
2023-08-15 10:44:40 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,1 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-#include <stdio.h>
-/* betty style doc for function main goes there */
-/**
-*main - Entry point
-*
-*Return: Always 0 (Success)
-*/
-int main(void)
-{
-	/* your code goes there */
-	printf("_putchar\n");
-	return (0);
-}
 
