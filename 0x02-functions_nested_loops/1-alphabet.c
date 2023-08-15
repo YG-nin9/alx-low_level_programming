5a48bc4bf962763ef2473f6c38fbed6023de4115
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-15 03:53:23 -0700

    put alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+void alphapet(void)
+{
+	/* your code goes there */
+	char out;
+	int count
+
+	for (out = 'a'; out <= 'z'; count++)
+	{
+		_putchar(out);
+	}
+	_putchar('\n');
+	return (0);
+}
+
