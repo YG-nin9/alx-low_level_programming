31b20b76790a182c51d28dfcaac672acf5783c50
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-15 06:58:58 -0700

    signs??

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,18 +13,18 @@ int print_sign(int n)
 	/* your code goes there */
 	if (n > 0)
 	{
+		putchar ('+');
 		return (1);
-		putchar('+');
 	}
 	else if (n == 0)
 	{
+		putchar ('0');
 		return (0);
-		putchar('0');
 	}
 	else 
 	{
+		putchar ('-');
 		return(-1);
-		putchar('-');
 	}
 }
 
