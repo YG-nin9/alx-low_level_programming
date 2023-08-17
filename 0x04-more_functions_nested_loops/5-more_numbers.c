e3befe7f53fbd9a999d9abee5d818c964c780f7a
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-17 05:02:21 -0700

    0 to 14 x10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/**
+*more_numbers - print numbs from 0 to 14 x10 times
+*Return: void
+*/
+void more_numbers(void)
+{
+	/* your code goes there */
+	char i;
+	int count;
+
+	while (count <= 10)
+	{
+		for (i = '0'; i <= '14'; i++)
+		{
+			_putchar (i);
+		}
+	_putchar('\n');
+	count++;
+	}
+}
+
