ed133d1deb3f74f7e2fb4699fc68d200dcab750c
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-17 05:29:22 -0700

    line up

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/**
+*print_line - print a line
+*@n: the length of the line
+*Return: void
+*/
+void print_line(int n)
+{
+	/* your code goes there */
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar ('_');
+		}
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+	_putchar('\n');
+}
+
