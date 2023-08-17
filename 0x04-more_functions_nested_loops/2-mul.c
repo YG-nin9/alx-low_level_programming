5135c73c2fec556bbc6ebd9af7a5fff1fd5959a5
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-17 03:43:43 -0700

    mul it

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/**
+*mul - get the multiply result of two numbers
+*@a: the first input
+*@b: the second inpput
+*Return: the result
+*/
+int mul(int a, int b)
+{
+	/* your code goes there */
+	int result;
+
+	result = a * b;
+	return (result);
+
+}
+
