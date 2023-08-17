72b285d399df7b8245ff02ecfa33b6f38086aa53
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-17 07:08:46 -0700

    test that

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,31 +4,31 @@
  * Return: out
  */
 
-int main()
+int main(void)
 {
 	int num;
 	int zero;
 
-	for (num = 0;num <= 100;num++)
+	for (num = 0; num <= 100; num++)
 	{
 		zero = 0;
 		if ((num % 3 == zero) && (num % 5 == zero) && (num != 0))
 		{
-			printf ("FizzBuzz ");
+			printf("FizzBuzz ");
 		}
 		else if ((num % 3 == 0) && (num != 0))
 		{
-			printf ("Fizz ");
+			printf("Fizz ");
 		}
 		else if ((num % 5 == 0) && (num != 0))
 		{
-			printf ("Buzz ");
+			printf("Buzz ");
 		}
 		else
 		{
 			printf("%d ", num);
 		}
 	}
-	printf ("\n");
+	printf("\n");
 	return (0);
 }
