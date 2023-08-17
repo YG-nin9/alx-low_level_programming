21d66ec9d15357d9197396d386e0cc94f2d28cf9
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-17 07:02:27 -0700

    test that

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - jus a programming test
+ * Return: out
+ */
+
+int main()
+{
+	int num;
+	int zero;
+
+	for (num = 0;num <= 100;num++)
+	{
+		zero = 0;
+		if ((num % 3 == zero) && (num % 5 == zero) && (num != 0))
+		{
+			printf ("FizzBuzz ");
+		}
+		else if ((num % 3 == 0) && (num != 0))
+		{
+			printf ("Fizz ");
+		}
+		else if ((num % 5 == 0) && (num != 0))
+		{
+			printf ("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf ("\n");
+	return (0);
+}
