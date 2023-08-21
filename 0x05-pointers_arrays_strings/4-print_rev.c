a8fc023c2988375d32cbb991030c59a856501cdf
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-21 13:11:49 -0700

    reve

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/**
+*print_rev - reverse that word
+*@s: the input
+*/
+
+void print_rev(char *s)
+{
+	/* your code goes there */
+
+	while (*s != '\0')
+	{
+		s++;
+	}
+	for (; s == 0;)
+	{
+		_putchar(*s--);
+	}
+	_putchar('\n');
+}
+
