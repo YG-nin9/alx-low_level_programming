9af2d18a054d6ab8902b926ddad0bffaaa5484c4
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-21 05:45:54 -0700

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/**
+*reset_to_98 - channges the value to 98
+*@*n: the input
+*@n: the value
+*/
+
+void reset_to_98(int *n)
+{
+	/* your code goes there */
+
+	*n = 98;
+}
+
