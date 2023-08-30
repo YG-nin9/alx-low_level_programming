d5b33766c49d0e3db4d7f2e85d0eac151c05e7e6
YG-nin9 <youssef.ghatas20@gmail.com>
2023-08-30 05:03:58 -0700

    rec rec

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - it is in the name bud
+ * @s: what will be put in rev hehehe
+ * Return: void ?
+ */
+void _print_rev_recursion(char *s)
+{	
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+
