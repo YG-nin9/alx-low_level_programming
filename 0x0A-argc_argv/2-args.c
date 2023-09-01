15f3ee410c2f9bae6f06a0f3501de26f08419d9d
YG-nin9 <youssef.ghatas20@gmail.com>
2023-09-01 08:04:32 -0700

    spell out loud

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the number of arguments
+ * @argv: the program name
+ * @argc: the counter for the variables
+ * Return: 0 in if no fail
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+	
+	n = 0;
+	while (n != argc)
+	{
+		printf("%s\n", *argv);
+		argv++;
+		n++;
+	}
+	return  (0);
+}
+
