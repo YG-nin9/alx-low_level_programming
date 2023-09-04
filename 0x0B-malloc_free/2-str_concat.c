4cce902d3d51d35197d203a88b1b8b196bbee52d
YG-nin9 <youssef.ghatas20@gmail.com>
2023-09-04 13:14:10 -0700

    free ma guy

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,37 +14,27 @@ char *str_concat(char *s1, char *s2)
 	len = 0;
 	x = 0;
 	n = 0;
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	}
-	while (s1[len] != '\0')
-	{
+	else if (s1 == NULL)
+		return (s2);
+	else if (s2 == NULL)
+		return (s1);
+	while (s1[len] != '\0' && (s1 != NULL))
 		len++;
-	}
 	len--;
-	while (s2[x] != '\0')
-	{
+	while (s2[x] != '\0' && (s2 != NULL))
 		x++;
-	}
-	y = x + len;
+	y = x + len + 1;
 	arr = malloc(sizeof(char) * y);
 	if (arr == NULL)
-	{
 		return (NULL);
-	}
 	for (m = 0; m <= y; m++)
-	{
 		if (m <= len)
-		{
 			arr[m] = s1[m];
-		}
 		else
-		{
-			arr[m] = s2[n];
-			n++;
-		}
-	}
+			{arr[m] = s2[n];
+			n++; }
 	return (arr);
 	free(arr);
 }
