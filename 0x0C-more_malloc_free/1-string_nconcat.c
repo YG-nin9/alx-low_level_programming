abeb704d8aa5e04372669ec2c702fb78702d87b2
YG-nin9 <youssef.ghatas20@gmail.com>
2023-09-07 04:53:20 -0700

    more of

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		x++;
 	if (n >= x)
 		n = x;
-	y = n + len + 1;
+	y = n + len;
 	arr = malloc(sizeof(char) * y);
 	if (arr == NULL)
 		return (NULL);
