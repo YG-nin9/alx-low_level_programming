4ba162c3713610515a73c3b22486bedce2573a04
YG-nin9 <youssef.ghatas20@gmail.com>
2023-09-26 07:13:44 -0700

    try hard

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_listint - free the heap
+ * @head: double pointer to the listint_t list
+ *
+ * Return: nothing i am a void u fool
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *holds;
+
+	while (head)
+	{
+		holds = head->next;
+		free(head);
+		head = holds;
+	}
+	
+}
