80cfc2d775334f5f355d85669573434cff837088
YG-nin9 <youssef.ghatas20@gmail.com>
2023-09-26 07:37:10 -0700

    try hard

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,18 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *hold;
 
-	while (head)
+	if (head == NULL)
 	{
-		free(head);
-		head = NULL;
+		return;
 	}
+
+	while (*head)
+	{
+		hold = (*head)->next;
+		free(*head);
+		*head = hold;
+	}
+	*head = NULL;
 }
