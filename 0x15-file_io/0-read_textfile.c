da738f5f91ee4d81af65bdd0a59f750390dd1d71
YG-nin9 <youssef.ghatas20@gmail.com>
2023-10-02 02:21:43 -0700

    wordplay

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * read_textfile - read the textfile then print it back
+ * @filename: pointer to the filename
+ * @ letters: the nmber of letters to be printed after read
+ * Return: number letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * letters);
+	t = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, t);
+
+	free(buffer);
+	close(fd);
+	return (w);
+
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+pre the shell beast 
+need to do good in that task since it is important to the project
