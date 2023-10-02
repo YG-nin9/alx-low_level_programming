09cd91c6c8ad413ec317ded6d5a9531c88fce557
YG-nin9 <youssef.ghatas20@gmail.com>
2023-10-02 03:06:22 -0700

    word play

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - create the textfile
+ * @filename: pointer to the filename
+ * @text_content: pointer to the text to be put into the file
+ * Return:
+ */
+create_file(const char *filename, char *text_content)
+{
+	int fd, w, len;
+
+	len = 0;
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (; textcontent[len];)
+		{
+			len++;
+		}
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+	if (fd == -1 || w == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
+
