4d00dab7d1c611517184a209e6f85a4d1723ffef
YG-nin9 <youssef.ghatas20@gmail.com>
2023-10-02 06:06:00 -0700

    word play

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @text_content: pointer to the text to be put into the file
  * Return: 1 on success -1 on fail
  */
-create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len;
 
